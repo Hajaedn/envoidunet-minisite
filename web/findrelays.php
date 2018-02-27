a3bbc4845db27f7afb4ee4c9f294fd7c6796d76d
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-02-27 14:23:10 +0100

    find relays, fix config

---
+++ web/findrelays.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'config.php';
+
+$client = new SoapClient('http://api.envoidunet.com/?wsdl');
+
+$login = new stdClass();
+$login->api_account = $config['api_account'];
+$login->api_key = $config['api_key'];
+
+$params = new stdClass();
+$params->carrier = 'dpd_relay';
+$params->postcode = '06700';
+$params->city = 'Saint Laurent du Var';
+$params->country = 'FR';
+
+try {
+    $result = $client->findRelays($login, $params);
+    if ($result['error']->error_id > 0) {
+        $error = $result['error'];
+        $message = $error->error_message;
+        $message .= isset($error->error_description) ? ' ('.$error->error_description.')' : '';
+        echo $message."\n";
+    }
+
+    return $result;
+
+} catch (SoapFault $e) {
+    echo $e->getMessage()."\n";
+}
+
